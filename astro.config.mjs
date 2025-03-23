476eb50c95c0999d74631cfbcea63156ce7c5bf8
Nolan Welch <nolanwelch@outlook.com>
2025-03-22 20:13:11 -0400

    Create site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Remove the tailwind integration since we're using the Vite plugin
+});

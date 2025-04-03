643320dbe719631f52c985f9c3a42d7436cc4fcf
Nolan Welch <nolanwelch@outlook.com>
2025-04-02 22:44:34 -0400

    Fix various Terraform issues

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,19 +1,3 @@
-resource "cloudflare_record" "root_cname" {
-  zone_id = var.zone_id
-  name    = "nolanwelch.com"
-  type    = "CNAME"
-  content = var.pages_domain
-  proxied = true
-}
-
-resource "cloudflare_record" "www" {
-  zone_id = var.zone_id
-  name    = "www"
-  type    = "CNAME"
-  content = var.pages_domain
-  proxied = true
-}
-
 resource "cloudflare_record" "track" {
   zone_id = var.zone_id
   name    = "track"
--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,10 +1,10 @@
-resource "cloudflare_worker_route" "track_subdomain" {
+resource "cloudflare_workers_route" "track_subdomain" {
   zone_id     = var.zone_id
   pattern     = "track.${var.domain}/*"
   script_name = "referral-tracker"
 }
 
-resource "cloudflare_worker_route" "track_dev_subdomain" {
+resource "cloudflare_workers_route" "track_dev_subdomain" {
   zone_id     = var.zone_id
   pattern     = "track-dev.${var.domain}/*"
   script_name = "referral-tracker"

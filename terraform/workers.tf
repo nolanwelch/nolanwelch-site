f68bf8341649e8664afd8369251b9978f789504f
Nolan Welch <nolanwelch@outlook.com>
2025-04-03 01:21:31 -0400

    Move Cloudflare Workers setup entirely into Terraform

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,3 +1,9 @@
+resource "cloudflare_workers_script" "referral_tracker" {
+  account_id = var.account_id
+  name       = "referral-tracker-production"
+  content    = file("workers/index.js")
+}
+
 resource "cloudflare_workers_route" "track_subdomain" {
   zone_id     = var.zone_id
   pattern     = "track.${var.domain}/*"

977e97f3c98b22bb9707ace1c920c56b726056d5
Nolan Welch <nolanwelch@outlook.com>
2025-04-03 01:25:50 -0400

    Add explicit dependency for tracking route

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -5,6 +5,7 @@ resource "cloudflare_workers_script" "referral_tracker" {
 }
 
 resource "cloudflare_workers_route" "track_subdomain" {
+  depends_on  = [cloudflare_workers_script.referral_tracker]
   zone_id     = var.zone_id
   pattern     = "track.${var.domain}/*"
   script_name = "referral-tracker"

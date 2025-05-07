a9833648c008e704ee6f8e5c4eb92fc25486a3bd
Nolan Welch <nolanwelch@outlook.com>
2025-05-07 22:11:33 +0000

    Make site mobile-friendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,13 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // Remove the tailwind integration since we're using the Vite plugin
+  vite: {
+    server: {
+      hmr: {
+        protocol: "ws",
+        host: "localhost",
+        port: 4321,
+      },
+    },
+  },
 });
